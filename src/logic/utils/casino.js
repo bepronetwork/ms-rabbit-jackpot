abfe58b808aed09ea528dc073c84f5dabefb4092
Ruiub3i <ruivaz9@hotmail.com>
2020-05-15 18:32:37 +0100

    update

--- src/logic/utils/casino.js
+++ src/logic/utils/casino.js
@@ -41,11 +41,9 @@ class CasinoLogic{
         resultSpaceArray.map( (item) => {
             i++;
             if(outcome >= item.start && outcome < item.end){
-                console.log("here", outcome, item.start, item.end)
                 res = item;
             }
         })
-        console.log("i", i)
         return res;
     }
     
